7eddaec3c90fbcc9eda8e108b4c5aa467ebd9219
danwritecode <dnelson4993@outlook.com>
2022-04-08 12:58:08 -0400

    started building out more pages

--- pages/index.vue
+++ pages/index.vue
@@ -3,10 +3,28 @@
     <div>
       <label for="passcode" class="block text-sm font-medium text-slate-700">Passcode</label>
       <div class="mt-1">
-        <input type="password" name="passcode" id="passcode" class="shadow-sm focus:ring-slate-500 focus:border-slate-500 block w-full sm:text-sm border-slate-300 rounded-md transition-hover-300" placeholder="supersecretcode">
+        <input v-model="passcode" type="password" name="passcode" id="passcode" class="shadow-sm focus:ring-slate-500 focus:border-slate-500 block w-full sm:text-sm border-slate-300 rounded-md transition-hover-300" placeholder="supersecretcode">
       </div>
 
-      <button class="mt-3 inline-flex items-center justify-center font-medium bg-slate-200 text-slate-900 py-3 w-full rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-slate-500 hover:bg-slate-300 transition-hover-300">go</button>
+      <button @click="submit()" class="mt-3 inline-flex items-center justify-center font-medium bg-slate-200 text-slate-900 py-3 w-full rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-slate-500 hover:bg-slate-300 transition-hover-300">go</button>
     </div>
   </div>
 </template>
+
+<script setup lang="ts">
+import { ref } from 'vue'
+import { useRouter, useRoute } from 'vue-router'
+
+const router = useRouter()
+
+const passcode = ref<string>("")
+
+let submit = () => {
+  if(passcode.value === "foo123")
+  {
+    router.push("/trips")
+  }
+}
+
+
+</script>
